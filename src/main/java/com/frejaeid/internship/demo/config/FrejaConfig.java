a1ab58f0c3705e83884957cc8eb29d480eed129d
matejadb <92729211+matejadb@users.noreply.github.com>
2026-04-05 14:11:28 +0200

    first commit

---
+++ src/main/java/com/frejaeid/internship/demo/config/FrejaConfig.java
@@ -0,0 +1,25 @@
+package com.frejaeid.internship.demo.config;
+
+import com.verisec.frejaeid.client.beans.general.SslSettings;
+import com.verisec.frejaeid.client.client.api.AuthenticationClientApi;
+import com.verisec.frejaeid.client.client.impl.AuthenticationClient;
+import com.verisec.frejaeid.client.enums.FrejaEnvironment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FrejaConfig {
+    @Value("${freja.keystore.password}")
+    private String keystorePassword;
+
+    @Bean
+    public AuthenticationClientApi authenticationClientApi() throws Exception {
+        String keystorePath = new java.io.File(AuthenticationClientApi.class.getClassLoader().getResource("FREJA_BG_Intern_2026-04-02.pfx").toURI()).getAbsolutePath();
+        String certPath = new java.io.File(AuthenticationClientApi.class.getClassLoader().getResource("freja_test_root_ca.crt").toURI()).getAbsolutePath();
+
+        SslSettings sslSettings = SslSettings.create(keystorePath, keystorePassword, certPath);
+
+        return AuthenticationClient.create(sslSettings, FrejaEnvironment.TEST).build();
+    }
+}
